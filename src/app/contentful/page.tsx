14f03ba85360408dafb09681487f6c810caf7b19
antocreadev <anthonymenghi@protonmail.com>
2024-04-26 17:27:38 +0200

    test

--- src/app/contentful/page.tsx
+++ src/app/contentful/page.tsx
@@ -1,7 +1,7 @@
-import { fetchData } from "@/functions/fetchContenful"
+// import { fetchData } from "@/functions/fetchContenful"
 export default async function ContentfulPage() {
-  const data = await fetchData("OHeTqrqztrTtHeahNNMnU"); 
-  console.log(data)
+  // const data = await fetchData("OHeTqrqztrTtHeahNNMnU"); 
+  // console.log(data)
   return (
     <div>page</div>
   )
